8a8e1d8d6c2d2b45236ff7092ac11f03fbab4bed
Lothar Rubusch <l.rubusch@gmail.com>
2021-10-29 23:45:13 +0200

    docker: fixed build.sh and cleanup
    Signed-off-by: Lothar Rubusch <l.rubusch@gmail.com>

--- docker/build_context/build.sh
+++ docker/build_context/build.sh
@@ -2,6 +2,7 @@
 MY_USER="$(whoami)"
 MY_HOME="/home/${MY_USER}"
 BUILDROOT_DIR="${MY_HOME}/buildroot"
+BRANCH="lothar/2020.11.x"
 DEFCONFIG=lothars__raspberrypi4_defconfig
 
 ## prepare
@@ -13,13 +14,13 @@ if [ -z "${FIRST}" ]; then
     cd "${MY_HOME}"
 
     ## NB: since we mount 'dl' and 'output', it needs a patched version of buildroot
-    RUN git clone -j4 --depth=1 --branch ${BRANCH} https://github.com/Rubusch/buildroot.git buildroot
+    git clone -j4 --depth=1 --branch ${BRANCH} https://github.com/Rubusch/buildroot.git buildroot
     ## alternatively get official buildrooot
-    #RUN git clone -j4 --depth=1 https://github.com/buildroot/buildroot.git buildroot
+    #git clone -j4 --depth=1 https://github.com/buildroot/buildroot.git buildroot
 fi
 
 cd ${BUILDROOT_DIR}
 make defconfig ${DEFCONFIG} || exit 1
-make -j8 || exit 1
+make -j "$(nproc)" || exit 1
 
 echo "READY."
